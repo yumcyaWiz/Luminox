17a6e6a330b5368d4e16f1a9db7166bfb3a5bb00
yumcyawiz <hashdolf1923@gmail.com>
2019-06-09 19:29:12 +0900

    add vec3

---
+++ vec3.h
@@ -0,0 +1,82 @@
+#ifndef VEC3_H
+#define VEC3_H
+#include <cmath>
+#include <iostream>
+class Vec3 {
+  public:
+    float x;
+    float y;
+    float z;
+
+    Vec3() { x = y = z = 0; };
+    Vec3(float _x) { x = y = z = _x; };
+    Vec3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {};
+
+    float length() const {
+      return std:sqrt(x*x + y*y + z*z);
+    };
+    float length2() const {
+      return x*x + y*y + z*z;
+    };
+};
+inline Vec3 operator+(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+inline Vec3 operator+(const Vec3& v1, float k) {
+  return Vec3(v1.x + k, v1.y + k, v1.z + k);
+}
+inline Vec3 operator+(float k, const Vec3& v2) {
+  return v2 + k;
+}
+
+
+inline Vec3 operator-(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+inline Vec3 operator-(const Vec3& v1, float k) {
+  return Vec3(v1.x - k, v1.y - k, v1.z - k);
+}
+inline Vec3 operator-(float k, const Vec3& v2) {
+  return Vec3(k - v2.x, k - v2.y, k - v2.z);
+}
+
+
+inline Vec3 operator*(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+}
+inline Vec3 operator*(const Vec3& v1, float k) {
+  return Vec3(v1.x * k, v1.y * k, v1.z * k);
+}
+inline Vec3 operator*(float k, const Vec3& v2) {
+  return v2 * k;
+}
+
+
+inline Vec3 operator/(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
+}
+inline Vec3 operator/(const Vec3& v1, float k) {
+  return Vec3(v1.x / k, v1.y / k, v1.z / k);
+}
+inline Vec3 operator/(float k, const Vec3& v2) {
+  return Vec3(k / v2.x, k / v2.y, k / v2.z);
+}
+
+
+inline std::ostream operator<<(std::ostream& stream, const Vec3& v) {
+  stream << "(" << v.x << ", " << v.y << ", " << v.z << ")" << std::endl;
+}
+
+
+inline float dot(const Vec3& v1, const Vec3& v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+inline Vec3 cross(const Vec3& v1, const Vec3& v2) {
+  return Vec3(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
+}
+
+
+inline Vec3 normalize(const Vec3& v) {
+  return v/v.length();
+}
+#endif
