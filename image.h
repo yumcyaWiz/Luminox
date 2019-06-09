d6525ceacea1ab775e31beeebca049a6eab809ac
yumcyawiz <hashdolf1923@gmail.com>
2019-06-09 19:40:01 +0900

    add image

---
+++ image.h
@@ -0,0 +1,54 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include "vec3.h"
+
+
+float clamp(float x, float xmin, float xmax) {
+  if(x < xmin) return xmin;
+  else if(x > xmax) return xmax;
+  else return x;
+}
+
+
+class Image {
+  public:
+    int width;
+    int height;
+    Vec3* pixels;
+
+    Image(int _width, int _height) : width(_width), height(_height) {
+      pixels = new Vec3[width*height];
+    };
+    ~Image() {
+      delete[] pixels;
+    };
+
+    Vec3 getPixel(int i, int j) const {
+      return pixels[j + i*width];
+    };
+    void setPixel(int i, int j, const Vec3& c) {
+      pixels[j + i*width] = c;
+    };
+
+    void ppm_output(const std::string& filename) const {
+      std::ofstream file(filename);
+      file << "P3" << std::endl;
+      file << width << " " << height << std::endl;
+      file << "255" << std::endl;
+
+      for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++) {
+          Vec3 c = this->getPixel(i, j);
+          int r = clamp(255*std::pow(c.x, 1/2.2), 0, 255);
+          int g = clamp(255*std::pow(c.y, 1/2.2), 0, 255);
+          int b = clamp(255*std::pow(c.z, 1/2.2), 0, 255);
+          file << r << " " << g << " " << b << std::endl;
+        }
+      }
+    };
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "image.h"
+
+
+int main() {
+  Image img(512, 512);
+  for(int i = 0; i < img.height; i++) {
+    for(int j = 0; j < img.width; j++) {
+      img.setPixel(i, j, Vec3(float(i)/img.height, float(j)/img.width, 1.0));
+    }
+  }
+  img.ppm_output("output.ppm");
+}
