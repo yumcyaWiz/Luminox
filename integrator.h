39ce52079b7109f3e4c0441e911a9f1ef672dd3e
yumcyawiz <hashdolf1923@gmail.com>
2019-06-12 05:37:18 +0900

    add NEE

--- integrator.h
+++ integrator.h
@@ -97,4 +97,79 @@ class PurePathTracing : public Integrator {
       image->ppm_output("output.ppm");
     };
 };
+
+
+class NEEPathTracing : public Integrator {
+  public:
+    int samples;
+    int maxDepth;
+
+    NEEPathTracing(const std::shared_ptr<Image>& _image, const std::shared_ptr<Camera>& _camera, const std::shared_ptr<Sampler>& _sampler, int _samples, int _maxDepth=100) : Integrator(_image, _camera, _sampler), samples(_samples), maxDepth(_maxDepth) {};
+
+    Vec3 integrate(const Ray& initRay, const Scene& scene) {
+      Ray ray = initRay;
+      Vec3 throughput(1, 1, 1);
+      float roulette = 1;
+      Vec3 accumulated_color(0, 0, 0);
+
+      for(int i = 0; i < maxDepth; i++) {
+        //Russian Roulette
+        if(sampler->getNext() > roulette) {
+          break;
+        }
+        else {
+          throughput /= roulette;
+          roulette *= 0.99;
+        }
+
+        Hit res;
+        if(scene.intersect(ray, res)) {
+          //Corner Case
+          if(i == 0 && res.hitPrimitive->hasLight()) {
+            accumulated_color += res.hitPrimitive->light->Le();
+          }
+
+          //Generate Local Coordinate Vectors
+          Vec3 n = res.hitNormal;
+          Vec3 s, t;
+          orthonormalBasis(n, s, t);
+          Vec3 wo = -ray.direction;
+          Vec3 wo_local = world2local(wo, s, n, t);
+
+          //light sampling
+          for(auto l : scene.lights) {
+            Vec3 sampledPos;
+            Vec3 normal;
+            float pdf_A;
+            Vec3 le = l->sample(res, *sampler, sampledPos, normal, pdf_A);
+
+            Vec3 wi = normalize(sampledPos - res.hitPos);
+            float pdf_w = (sampledPos - res.hitPos).length2() / std::abs(dot(-wi, normal)) * pdf_A;
+
+            Vec3 wi_local = world2local(wi, s, n, t);
+            Vec3 brdf = res.hitPrimitive->material->BRDF(res, wo_local, wi_local);
+            accumulated_color += throughput * brdf * absCosTheta(wi_local) * le / pdf_w;
+          }
+
+          //BRDF sampling for next ray
+          auto mat = res.hitPrimitive->material;
+          Vec3 wi_local;
+          float pdf_w;
+          Vec3 brdf_value = mat->sample(wo_local, res, *sampler, wi_local, pdf_w);
+
+          //Update throughput
+          throughput *= brdf_value * absCosTheta(wi_local) / pdf_w;
+
+          //Generate Next Ray
+          Vec3 wi = local2world(wi_local, s, n, t);
+          ray = Ray(res.hitPos, wi);
+        }
+        else {
+          accumulated_color += throughput * Vec3(0, 0, 0);
+          break;
+        }
+      }
+      return accumulated_color;
+    };
+};
 #endif
